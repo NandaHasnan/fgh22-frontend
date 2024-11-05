2e72289b85e21b9643879c6533c4d69398e06529
NandaHasnan <nandahasnan6@gmail.com>
2024-11-05 08:50:16 +0700

    Revisi v3

--- src/page/payment.jsx
+++ src/page/payment.jsx
@@ -1,128 +1,125 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Google from '../assets/gambar/logos_google-pay.png'
-import Visa from '../assets/gambar/logos_visa.png'
-import Gopay from '../assets/gambar/Logo GoPay (SVG-240p) - FileVector69 1.png'
-import Paypal from '../assets/gambar/logos_paypal.png'
-import Dana from '../assets/gambar/Logo DANA (PNG-240p) - FileVector69 1.png'
-import Bca from '../assets/gambar/Bank BCA Logo (SVG-240p) - FileVector69 1.png'
-import Bri from '../assets/gambar/bri.png'
-import Ovo from '../assets/gambar/ovo.png'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+import Google from '../assets/gambar/logos_google-pay.png';
+import Visa from '../assets/gambar/logos_visa.png';
+import Gopay from '../assets/gambar/Logo GoPay (SVG-240p) - FileVector69 1.png';
+import Paypal from '../assets/gambar/logos_paypal.png';
+import Dana from '../assets/gambar/Logo DANA (PNG-240p) - FileVector69 1.png';
+import Bca from '../assets/gambar/Bank BCA Logo (SVG-240p) - FileVector69 1.png';
+import Bri from '../assets/gambar/bri.png';
+import Ovo from '../assets/gambar/ovo.png';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 import { FaTimes } from 'react-icons/fa';
 import Step from '../components/step-active';
 import Step2 from '../components/step-oren';
 
 function App() {
-   useState(0)
+   useState(0);
    const [isShow, setShow] = useState(false);
+
   return (
-    <div className=''>
-      <Navbar/>
-      <main className='py-8 px-48 flex flex-col gap-10 bg-[#A0A3BD20]'>
-        <div className='gap-6 flex justify-center items-center mb-8'>
-            <Step status='active' content='Date and time'/>
-            <div className="w-16 border border-abu border-dashed"></div>
-            <Step status='active' content='Seat'/>
-            <div className="w-16 border border-abu border-dashed"></div>
-            <Step2 status='not' content='Payment' num='3'/>
+    <div className="">
+      <Navbar />
+      <main className="py-8 px-4 md:px-16 lg:px-24 xl:px-48 flex flex-col gap-10 bg-[#A0A3BD20]">
+        <div className="hidden gap-6 md:flex justify-center items-center mb-8">
+          <Step status="active" content="Date and time" />
+          <div className="w-16 border border-abu border-dashed"></div>
+          <Step status="active" content="Seat" />
+          <div className="w-16 border border-abu border-dashed"></div>
+          <Step2 status="not" content="Payment" num="3" />
         </div>
 
-        <section className='flex justify-center gap-4'>
-            
-            <div className=''>
-                <div className='flex flex-col gap-9 w-[732px] h-[1615px] bg-white px-8 py-12 rounded-md'>
-                    <div className='text-2xl text-[#14142B] font-semibold'>Payment Info</div>
-                    <div className='flex flex-col gap-8'>
-                        <div className='text-sm text-[#8692A6]'>DATE & TIME</div>
-                        <input className='text-base placeholder:text-[#000000]' type="text" id='date' name='date' placeholder='Tuesday, 07 July 2020 at 02:00pm' />
-                        {/* <div className='text-base'>Tuesday, 07 July 2020 at 02:00pm</div> */}
-                        <div className='w-95 h-[1px] bg-[#E6E6E6]'></div>
-                        <div className='text-sm text-[#8692A6]'>MOVIE TITLE</div>
-                        <input className='text-base placeholder:text-[#000000]' type="text" id='date' name='date' placeholder='Spider-Man: Homecoming' />
-                        {/* <div className='text-base'>Spider-Man: Homecoming</div> */}
-                        <div className='w-95 h-[1px] bg-[#E6E6E6]'></div>
-                        <div className='text-sm text-[#8692A6]'>CINEMA NAME</div>
-                        <input className='text-base placeholder:text-[#000000]' type="text" id='date' name='date' placeholder='CineOne21 Cinema' />
-                        {/* <div className='text-base'>CineOne21 Cinema</div> */}
-                        <div className='w-95 h-[1px] bg-[#E6E6E6]'></div>
-                        <div className='text-sm text-[#8692A6]'>NUMBER OF TICKETS</div>
-                        <input className='text-base placeholder:text-[#000000]' type="text" id='date' name='date' placeholder='3 pieces' />
-                        {/* <div className='text-base'>3 pieces</div> */}
-                        <div className='w-95 h-[1px] bg-[#E6E6E6]'></div>
-                        <div className='text-sm text-[#8692A6]'>TOTAL PAYMENT</div>
-                        <input className='text-base placeholder:text-[#1D4ED8] placeholder:font-semibold' type="text" id='date' name='date' placeholder='$30,00' />
-                        {/* <div className='text-base text-[#1D4ED8] font-semibold'>$30,00</div> */}
-                        <div className='w-95 h-[1px] bg-[#E6E6E6]'></div>
-                    </div>
-                    <div className='text-2xl text-[#14142B] font-semibold'>Personal Information</div>
-                    <form>
-                        <div className='flex flex-col gap-3.5'>
-                            <label className='text-base text-[#696F79]' htmlFor="full-name">Full Name</label>
-                            <input className='w-[665px] h-16 border rounded-md px-11 border-[#DEDEDE]' type="text" id='full-name' name='full-nama' placeholder='Jonas El Rodriguez'/>
-                            <label className='text-base text-[#696F79]' htmlFor="full-name">Full Name</label>
-                            <input className='w-[665px] h-16 border rounded-md px-11 border-[#DEDEDE]' type="text" id='full-name' name='full-nama' placeholder='Jonas El Rodriguez'/>
-                            <label className='text-base text-[#696F79]' htmlFor="full-name">Full Name</label>
-                            <input className='w-[665px] h-16 border rounded-md px-11 border-[#DEDEDE]' type="text" id='full-name' name='full-nama' placeholder='Jonas El Rodriguez'/>
-                        </div>
-                    </form>
-                    <div className='text-2xl text-[#14142B] font-semibold'>Personal Information</div>
-                    <div className='grid grid-cols-4 gap-6'>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Google} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Visa} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Gopay} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Paypal} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Dana} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Bca} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Bri} alt="" /></div>
-                        <div className='py-3 px-9 w-36 h-14 border border-[#DEDEDE] rounded-md'><img src={Ovo} alt="" /></div>
-                    </div>
-                    <button onClick={() =>setShow(!isShow)} className='w-[665px] h-14 bg-oren text-center rounded-md text-white'>Pay your order</button>
-                </div>
-            </div>
-            
-        </section>
-        
-        
-      </main>
-      {isShow && ( <>
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="flex flex-col gap-8 bg-white w-[573px] h-[506px] p-6 rounded-lg shadow-lg">
-              <button onClick={() =>setShow(!isShow)}><FaTimes className="text-2xl" /></button>
-              <div className="text-2xl font-semibold text-[#14142B] text-center">Payment Info</div>
-              <div className="flex items-center justify-between">
-                <div className='flex'>
-                  <label className="text-[#8692A6] text-sm">No. Rekening Virtual</label>
-                  <label className="text-[#8692A6] text-sm">:</label>
-                </div>
-                <div className="flex gap-5">
-                  <input type="text" readOnly placeholder="1232132031829734" className="w-40 rounded-l-md focus:outline-none text-lg font-semibold"/>
-                  <button className="border border-orenMuda text-oren px-3 py-2 rounded-lg text-sm">Copy</button>
-                </div>
+        <section className="flex justify-center gap-4 flex-wrap">
+          <div className="w-full md:w-auto">
+            <div className="flex flex-col gap-9 w-full md:w-[732px] bg-white px-6 md:px-8 py-8 md:py-12 rounded-md">
+              <div className="text-xl md:text-2xl text-[#14142B] font-semibold">Payment Info</div>
+              <div className="flex flex-col gap-6 md:gap-8">
+                <div className="text-sm text-[#8692A6]">DATE & TIME</div>
+                <input className="text-base placeholder:text-[#000000]" type="text" id="date" name="date" placeholder="Tuesday, 07 July 2020 at 02:00pm" />
+                <div className="w-full h-[1px] bg-[#E6E6E6]"></div>
+                <div className="text-sm text-[#8692A6]">MOVIE TITLE</div>
+                <input className="text-base placeholder:text-[#000000]" type="text" id="date" name="date" placeholder="Spider-Man: Homecoming" />
+                <div className="w-full h-[1px] bg-[#E6E6E6]"></div>
+                <div className="text-sm text-[#8692A6]">CINEMA NAME</div>
+                <input className="text-base placeholder:text-[#000000]" type="text" id="date" name="date" placeholder="CineOne21 Cinema" />
+                <div className="w-full h-[1px] bg-[#E6E6E6]"></div>
+                <div className="text-sm text-[#8692A6]">NUMBER OF TICKETS</div>
+                <input className="text-base placeholder:text-[#000000]" type="text" id="date" name="date" placeholder="3 pieces" />
+                <div className="w-full h-[1px] bg-[#E6E6E6]"></div>
+                <div className="text-sm text-[#8692A6]">TOTAL PAYMENT</div>
+                <input className="text-base placeholder:text-[#1D4ED8] placeholder:font-semibold" type="text" id="date" name="date" placeholder="$30,00" />
+                <div className="w-full h-[1px] bg-[#E6E6E6]"></div>
               </div>
-              <div className='flex items-center justify-between'>
-                <div className='flex'>
-                  <label className="text-[#8692A6] text-sm">Total Payment</label>
-                  <label className="text-[#8692A6] text-sm">:</label>
-                </div>
-                <input className='text-right text-lg placeholder:text-oren font-semibold' type="text" placeholder='$30'/>
+
+              <div className="text-xl md:text-2xl text-[#14142B] font-semibold">Personal Information</div>
+              <form className="flex flex-col gap-4 md:gap-3.5">
+                <label className="text-sm md:text-base text-[#696F79]" htmlFor="full-name">Full Name</label>
+                <input className="w-full md:w-[665px] h-12 md:h-16 border rounded-md px-4 md:px-11 border-[#DEDEDE]" type="text" id="full-name" name="full-nama" placeholder="Jonas El Rodriguez" />
+                <label className="text-sm md:text-base text-[#696F79]" htmlFor="email">Email</label>
+                <input className="w-full md:w-[665px] h-12 md:h-16 border rounded-md px-4 md:px-11 border-[#DEDEDE]" type="email" id="email" name="email" placeholder="jonas@mail.com" />
+                <label className="text-sm md:text-base text-[#696F79]" htmlFor="phone">Phone Number</label>
+                <input className="w-full md:w-[665px] h-12 md:h-16 border rounded-md px-4 md:px-11 border-[#DEDEDE]" type="tel" id="phone" name="phone" placeholder="+1 234 567 890" />
+              </form>
+
+              <div className="text-xl md:text-2xl text-[#14142B] font-semibold">Payment Method</div>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
+                {[Google, Visa, Gopay, Paypal, Dana, Bca, Bri, Ovo].map((imgSrc, index) => (
+                  <div key={index} className="flex justify-center items-center py-3 px-6 w-full h-12 md:w-36 md:h-14 border border-[#DEDEDE] rounded-md">
+                    <img src={imgSrc} alt="" />
+                  </div>
+                ))}
               </div>
-              <div className='text-base text-[#A0A3BD] leading-8'>Pay this payment bill before it is due, on <span className='text-base text-[#D00707]'>June 23, 2023</span>. If the bill has not been paid by the specified time, it will be forfeited</div>
-              <Link to='/ticket' className='py-3 w-[520px] rounded-md h-14 text-center font-semibold text-white bg-oren'>Check Payment</Link>
-              <Link className='py-3 w-[520px] h-14 text-center font-semibold text-oren'>Pay Later</Link>
+
+              <button onClick={() => setShow(!isShow)} className="w-full md:w-[665px] h-12 md:h-14 bg-oren text-center rounded-md text-white">
+                Pay your order
+              </button>
             </div>
           </div>
-          </>
-                        
-        )}
+        </section>
+      </main>
 
-      <footer className='px-48 flex flex-col gap-12'>
-        <Footer/>
-        </footer>
-      
+      {isShow && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="flex flex-col gap-8 bg-white w-full max-w-lg md:w-[573px] h-auto md:h-[506px] p-6 rounded-lg shadow-lg">
+            <button onClick={() => setShow(!isShow)}><FaTimes className="text-2xl" /></button>
+            <div className="text-xl md:text-2xl font-semibold text-[#14142B] text-center">Payment Info</div>
+            <div className="flex items-center justify-between">
+              <div className="flex">
+                <label className="text-[#8692A6] text-sm">No. Rekening Virtual</label>
+                <span className="ml-1 text-[#8692A6] text-sm">:</span>
+              </div>
+              <div className="flex gap-3">
+                <input type="text" readOnly placeholder="1232132031829734" className="w-40 rounded-l-md focus:outline-none text-lg font-semibold" />
+                <button className="border border-orenMuda text-oren px-3 py-2 rounded-lg text-sm">Copy</button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex">
+                <label className="text-[#8692A6] text-sm">Total Payment</label>
+                <span className="ml-1 text-[#8692A6] text-sm">:</span>
+              </div>
+              <input className="text-right text-lg placeholder:text-oren font-semibold" type="text" placeholder="$30" />
+            </div>
+            <div className="text-sm md:text-base text-[#A0A3BD] leading-7 md:leading-8">
+              Pay this payment bill before it is due, on <span className="text-sm md:text-base text-[#D00707]">June 23, 2023</span>.
+              If the bill has not been paid by the specified time, it will be forfeited.
+            </div>
+            <Link to="/ticket" className="py-3 w-full text-center font-semibold text-white bg-oren rounded-md">
+              Check Payment
+            </Link>
+            <Link to="/" className="py-3 w-full text-center font-semibold text-oren">
+              Pay Later
+            </Link>
+          </div>
+        </div>
+      )}
+
+      <footer className="px-4 md:px-16 lg:px-24 xl:px-48">
+        <Footer />
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/page/ticket-result.jsx
+++ src/page/ticket-result.jsx
@@ -1,73 +1,71 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo2 from '../assets/gambar/logo2.png'
-import Barcode from '../assets/gambar/barcode.png'
+import Logo2 from '../assets/gambar/logo2.png';
+import Barcode from '../assets/gambar/barcode.png';
 import { MdOutlineFileDownload } from "react-icons/md";
 import { HiArrowLongRight } from "react-icons/hi2";
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 function App() {
-   useState(0)
+   useState(0);
 
   return (
     <div className=''>
       <Navbar/>
-      <main className='flex flex-col lg:flex-row justify-between gap-10 bg-[#A0A3BD20]'>
+      <main className='flex flex-col lg:flex-row justify-between gap-10 bg-[#A0A3BD20] p-4 lg:p-0'>
         
-          <div className='bg-bg-marvel2 w-[1920px] '>
-            <div className='bg-[#00000090] w-[1319px] h-[920px]'>
-              <div className='px-28 pt-[334px]'>
-                    <div className=' flex flex-col gap-5'>
-                        <img className='w-64' src={Logo2} alt="" />
-                        <div className='text-5xl text-white font-semibold'>Thankyou For Purchasing</div>
-                        <div className='w-[571px] text-2xl text-white'>Lorem ipsum dolor sit amet consectetur. Quam pretium pretium tempor integer sed magna et.</div>
-                        <div className='text-lg text-white font-semibold flex items-center gap-6'>Please Download Your Ticket<HiArrowLongRight /></div>
+          <div className='bg-bg-marvel2 w-full lg:w-[60%] h-[590px] bg-center md:bg-normal bg-cover md:h-full'>
+            <div className='bg-[#00000090] w-full lg:w-full h-full lg:h-[920px] flex items-center'>
+              <div className='px-6 lg:px-28 pt-10 lg:pt-[60px]'>
+                    <div className='flex flex-col gap-5 justify-center items-center md:justify-start md:items-start text-center md:text-left'>
+                        <img className='w-48 lg:w-64' src={Logo2} alt="" />
+                        <div className='text-3xl lg:text-5xl text-white font-semibold'>Thank you For Purchasing</div>
+                        <div className='text-base lg:text-2xl text-[#FFFFFF70] w-full lg:w-[571px]'>Lorem ipsum dolor sit amet consectetur. Quam pretium pretium tempor integer sed magna et.</div>
+                        <div className='text-lg text-white font-semibold flex items-center gap-3 lg:gap-6'>Please Download Your Ticket<HiArrowLongRight /></div>
                     </div>
               </div>
             </div>
           </div>
 
-          <div className='pt-20 px-28 flex flex-col gap-6'>
-            <div className='flex flex-col gap-28 rounded-md w-80 h-[635px] bg-white'>
-                <div className='py-8 px-16'>
-                  <img className='w-44 h-44' src={Barcode} alt="" />
+          <div className=' pt-10 lg:pt-20 px-10 lg:px-28 flex flex-col gap-6 w-full lg:w-[40%]'>
+            <div className='flex flex-col gap-48 w-[327px] lg:gap-28 rounded-md lg:w-80 h-auto lg:h-[635px] bg-white p-6'>
+                <div className='flex justify-center py-8'>
+                  <img className='w-32 lg:w-44 h-32 lg:h-44' src={Barcode} alt="" />
                 </div>
-                <div className='flex flex-col gap-10'>
-                  <div className='flex flex-col gap-6'>
-                    <div className='flex px-10 justify-between'>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Movie</div>
-                        <div className='text-sm text-[#14142B] font-normal'>Spider-Man: ..</div>
-                      </div>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Category</div>
-                        <div className='text-sm text-[#14142B] font-normal'>PG-13</div>
-                      </div>
+                <div className='flex flex-col gap-6'>
+                  <div className='flex flex-wrap justify-between px-6'>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Movie</div>
+                      <div className='text-sm text-[#14142B] font-normal'>Spider-Man: ..</div>
                     </div>
-                    <div className='flex px-10 justify-between'>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Date</div>
-                        <div className='text-sm text-[#14142B] font-normal'>07 Jul</div>
-                      </div>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Time</div>
-                        <div className='text-sm text-[#14142B] font-normal'>2:00pm</div>
-                      </div>
-                    </div>
-                    <div className='flex px-10 justify-between'>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Count</div>
-                        <div className='text-sm text-[#14142B] font-normal'>3 pcs</div>
-                      </div>
-                      <div>
-                        <div className='text-xs text-[#AAAAAA]'>Seats</div>
-                        <div className='text-sm text-[#14142B] font-normal'>C4, C5, C6</div>
-                      </div>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Category</div>
+                      <div className='text-sm text-[#14142B] font-normal'>PG-13</div>
                     </div>
                   </div>
-                  <div className='px-10'>
-                    <div className='rounded-md py-2.5 px-6 w-60 h-12 border border-[#DEDEDE]'>
+                  <div className='flex flex-wrap justify-between px-6'>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Date</div>
+                      <div className='text-sm text-[#14142B] font-normal'>07 Jul</div>
+                    </div>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Time</div>
+                      <div className='text-sm text-[#14142B] font-normal'>2:00pm</div>
+                    </div>
+                  </div>
+                  <div className='flex flex-wrap justify-between px-6'>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Count</div>
+                      <div className='text-sm text-[#14142B] font-normal'>3 pcs</div>
+                    </div>
+                    <div>
+                      <div className='text-xs text-[#AAAAAA]'>Seats</div>
+                      <div className='text-sm text-[#14142B] font-normal'>C4, C5, C6</div>
+                    </div>
+                  </div>
+                  <div className='px-6'>
+                    <div className='rounded-md py-2.5 px-4 lg:px-6 w-full lg:w-60 h-12 border border-[#DEDEDE]'>
                       <div className='flex justify-between '>
                         <div>Total</div>
                         <div>$30.00</div>
@@ -76,20 +74,22 @@ function App() {
                   </div>
                 </div>
             </div>
-            <div className='flex flex-col gap-2.5'>
-              <Link className='flex w-[320px] h-14 rounded-md border border-orenMuda items-center justify-center gap-6 text-oren font-semibold'><MdOutlineFileDownload />Download</Link>
-              <Link to='/profil' className='w-[320px] h-14 rounded-md bg-oren text-center text-white font-semibold pt-3'>Done</Link>
+            <div className='flex flex-col gap-2.5 px-4 lg:px-0'>
+              <Link className='flex w-full lg:w-[320px] h-14 rounded-md border border-orenMuda items-center justify-center gap-4 lg:gap-6 text-oren font-semibold'>
+                <MdOutlineFileDownload />Download
+              </Link>
+              <Link to='/profil' className='w-full lg:w-[320px] h-14 rounded-md bg-oren text-center text-white font-semibold pt-3'>Done</Link>
             </div>
           </div>
         
       </main>
 
-      <footer className='px-48 flex flex-col gap-12'>
+      <footer className='px-6 lg:px-48 flex flex-col gap-12'>
           <Footer/>
-        </footer>
+      </footer>
       
     </div>
   )
 }
 
-export default App
+export default App;
